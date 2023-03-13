db2c6d48f3e0975306079acb197de0f2c84fc1ae
reczkok <66403540+reczkok@users.noreply.github.com>
2023-03-13 08:23:11 +0100

    Add files via upload

---
+++ Token.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleScanner;
+
+public class Token
+{
+    private readonly TokenType _type;
+    private readonly string? _text;
+
+    public Token(TokenType t, string? text)
+    {
+        _type = t;
+        _text = text;
+    }
+
+    public override string ToString()
+    {
+        return _text is null ? new string(_type.ToString()) : new string(_type + " " + _text);
+    }
+}
