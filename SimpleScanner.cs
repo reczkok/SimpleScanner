39f32a7bfa97a6bed4344bc2529e263b62ff7558
reczkok <66403540+reczkok@users.noreply.github.com>
2023-03-14 19:16:28 +0100

    Update SimpleScanner.cs

--- SimpleScanner.cs
+++ SimpleScanner.cs
@@ -1,14 +1,33 @@
-﻿namespace SimpleScanner;
-
-// TODO: Handle more exceptions
-// Currently only handles unexpected characters
-// Should probably handle invalid tokens as well (?)
-
+namespace SimpleScanner;
 
 public static class SimpleScanner
 {
+    private static void CheckValidity(TextReader input, char first, int idx)
+    {
+        switch (first)
+        {
+            case '+': case '*': case '-': case '/':
+                if (input.Peek() == -1 || input.Peek() == 114)
+                    throw new Exception("Inavlid end of input");
+                if (!(char.IsDigit((char)input.Peek()) || (char)input.Peek() == '('))
+                    throw new Exception("Invalid token: " + (char)input.Peek() + " at idx: " + (idx+1));
+                break;
+            case '(':
+                if (input.Peek() == -1 || input.Peek() == 114)
+                    throw new Exception("Inavlid end of input");
+                if(!(char.IsDigit((char)input.Peek()) || (char)input.Peek() == '-'))
+                    throw new Exception("Invalid token: " + (char)input.Peek() + " at idx: " + (idx+1));
+                break;
+            case ')':
+                if (input.Peek() == -1 || input.Peek() == 114)
+                    break;
+                if(char.IsDigit((char)input.Peek()))
+                    throw new Exception("Invalid token: " + (char)input.Peek() + " at idx: " + (idx+1));
+                break;
+        }
+    }
     // Function to handle multiple digit numbers
-    private static int MatchNumber(TextReader input, char first, List<Token> output)
+    private static int MatchNumber(TextReader input, char first, List<Token> output, int idx)
     {
         var wholeNumber = first.ToString();
         while(input.Peek() != -1 && char.IsDigit((char)input.Peek()))
@@ -16,6 +35,8 @@ public static class SimpleScanner
             wholeNumber += (char)input.Read();
         }
         output.Add(new Token(TokenType.Number, wholeNumber));
+        if ((char)input.Peek() == '(')
+            throw new Exception("Invalid token: " + (char)input.Peek() + " at idx: " + (idx + wholeNumber.Length));
         return wholeNumber.Length;
     }
 
@@ -30,30 +51,36 @@ public static class SimpleScanner
             var nextChar = (char)nextInput;
             // Skip whitespace
             if(nextChar == ' ') continue;
-            // The end of the input character
+            // The end of the input character 
             if (nextChar == 'r') return output;
             switch (nextChar)
             {
                 case '+':
                     output.Add(new Token(TokenType.Plus, null)); idx += 1;
+                    CheckValidity(input, nextChar, idx);
                     break;
                 case '-':
                     output.Add(new Token(TokenType.Minus, null)); idx += 1;
+                    CheckValidity(input, nextChar, idx);
                     break;
                 case '*':
                     output.Add(new Token(TokenType.Multiplication, null)); idx += 1;
+                    CheckValidity(input, nextChar, idx);
                     break;
                 case '/':
                     output.Add(new Token(TokenType.Division, null)); idx += 1;
+                    CheckValidity(input, nextChar, idx);
                     break;
                 case '(':
                     output.Add(new Token(TokenType.BracketOpen, null)); idx += 1;
+                    CheckValidity(input, nextChar, idx);
                     break;
                 case ')':
                     output.Add(new Token(TokenType.BracketClose, null)); idx += 1;
+                    CheckValidity(input, nextChar, idx);
                     break;
                 case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-                    idx += MatchNumber(input, nextChar, output);
+                    idx += MatchNumber(input, nextChar, output, idx);
                     break;
                 default:
                     throw new Exception("Unexpected character: " + nextChar + " at position " + idx + ".");
